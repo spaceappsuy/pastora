fac82f989bb755529d2ea4769e7eb58ce849d022
David Rivero <guest-developer@Juans-MacBook-Pro.local>
2016-04-24 00:23:17 -0300

    First commit. Setup application

---
+++ data_analysis/utils.py
@@ -0,0 +1,16 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
+
+
+class MLAnalysis():
+	def __init__(self):
+		pass
+
+	def fitdata(self, dataset):
+		pass
+
+	def validate(self):
+		pass
+
+	def predict(self, dataset):
+		pass
---
+++ data_process/utils.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+class ProcessCSV():
+	def __init__(self):
+		pass
+
+	def download_csv(self):
+		pass
+
+	def read_csv(self):
+		pass
+
+	def parse_data_to_numpy_arrays(self):
+		pass
