bbcc7f792e61c34208f62a35878e4f6f4f4331df
David Rivero <guest-developer@Juans-MacBook-Pro.local>
2016-04-29 14:10:07 -0300

    Fix files for Github

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z "$VCAP_APP_PORT" ];
-then SERVER_PORT=8000;
-else SERVER_PORT="$VCAP_APP_PORT";
-fi
-echo port is $SERVER_PORT
-python manage.py runserver --noreload 0.0.0.0:$SERVER_PORT
